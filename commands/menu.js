44f863ad8dbc34ddcfaf9d0fb028fbbd54380526
TheENGR <50523916+TheENGR@users.noreply.github.com>
2022-01-23 19:51:22 -0600

    Update and rename status.js to menu.js

--- commands/menu.js
+++ commands/menu.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('status')
+		.setName('menu')
 		.setDescription('Get the current plan for dinner'),
 	async execute(interaction) {	
 		return interaction.reply("ToDo: Print the plan for dinner");
