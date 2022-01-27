47c16bdd927bdc8168e5d24bd4c5651462be74e8
TheENGR <TheENGR42@gmail.com>
2022-01-27 16:30:45 -0600

    Add quantity and deletion of dishes

--- commands/assign-dish.js
+++ commands/assign-dish.js
@@ -5,28 +5,60 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('assign-dish')
 		.setDescription('assign a dish to a user')
+		.addStringOption(option => option.setName('dish').setDescription('Enter a dish').setRequired(true))
 		.addUserOption(option => option.setName('user').setDescription('Who is bringing the dish>'))
-		.addStringOption(option => option.setName('dish').setDescription('Enter a dish')),
+		.addNumberOption(option => option.setName('quantity').setDescription('How many?'))
+		.addBooleanOption(option => option.setName('delete').setDescription('Delete this dish')),
 	async execute(interaction) {
 		let user = interaction.options.getUser('user');
 		const dish = interaction.options.getString('dish');
+		const quantity = interaction.options.getNumber('quantity');
+		const deleteDish = interaction.options.getBoolean('delete');
 		let menu = await getMenu();
 		
-		if (dish == undefined) {
-			return interaction.reply(`No dish was provided to sign up for`);
-		}
+		let reply = [];
 		
-		if (menu.Dishes[dish] != undefined && menu.Dishes[dish] != '') {
-			return interaction.reply(`${dish} is already being brought by <@${menu.Dishes[dish]}>`)
-		}
-		
-		// Only the member in charge can assign other people dishes
-		if (menu.Owner == interaction.user.id || user == undefined || interaction.user.id == user.id) {
-			menu.Dishes[dish] = user == undefined ? '' : user.id
-			await saveMenu(menu);
-			return interaction.reply(`Added ${dish} to menu`)
+		if (deleteDish) {
+			let newDish = menu.Dishes[dish];
+			if (menu.Owner == interaction.user.id || newDish.Owner == undefined || newDish.Owner == user.id) {
+				delete menu.Dishes[dish];
+				await saveMenu(menu);
+				reply.push(`${dish} removed from menu`)
+			} else {
+				reply.push(`You cannot remove ${dish} from menu`)
+			}
 		} else {
-			return interaction.reply(`You are not authorized to sign <@${user.id}> up to bring ${dish}`);
+			// Deep copy the dish if it exists or just make an empty one
+			let newDish = menu.Dishes[dish] ? JSON.parse(JSON.stringify(menu.Dishes[dish])) : undefined;
+			if (newDish == undefined) {
+				newDish = {};
+				reply.push(`Added ${dish} to the menu`)
+			}
+			
+			// Assigning user and it is self or done by owner
+			if (user != undefined) {
+				if (menu.Owner == interaction.user.id || interaction.user.id == user.id) {
+					newDish.Owner = user.id;
+					reply.push(`<@${user.id}> assigned to bring ${dish}`)
+				} else {
+					reply.push("You cannot assign other people dishes")
+				}
+			}
+			
+			// Assigning quantity and it is unowned, my dish, or done by owner
+			if (quantity != undefined) {
+				if (menu.Owner == interaction.user.id || newDish.Owner == undefined || newDish.Owner == user.id) {
+					newDish.Quantity = quantity;
+					reply.push(`Quantity of ${dish} set to ${quantity}`)
+				} else {
+					reply.push("You cannot assign change the quantity of ${dish}")
+				}
+			}
+			
+			menu.Dishes[dish] = newDish;
+			await saveMenu(menu);
 		}
+		
+		return interaction.reply(reply.join('\n'));
 	},
 };
