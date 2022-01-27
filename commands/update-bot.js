d1026749abe98b05168ccef384064b0b10f1c6b9
TheENGR <TheENGR42@gmail.com>
2022-01-27 15:50:37 -0600

    Update command

--- commands/update-bot.js
+++ commands/update-bot.js
@@ -5,7 +5,7 @@ module.exports = {
 		.setName('update-bot')
 		.setDescription('Update the bot after a PR'),
 	async execute(interaction) {	
-		await interaction.reply("Bot updating, please wait...");
+		await interaction.reply("Bot updating, please wait for 15 seconds before next command");
 		await process.exit();
 	},
 };
--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-git pull
-git add menu-memory/memory.json
-git commit -m "Save Memory"
-git push 
-npm ci
-node deploy-commands.js
-node index.js
