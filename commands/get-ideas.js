4603aaa1778935555789b16a6acb9be83b44d9d6
TheENGR <50523916+TheENGR@users.noreply.github.com>
2022-03-22 18:48:52 -0500

    Update get-ideas.js

--- commands/get-ideas.js
+++ commands/get-ideas.js
@@ -1,19 +1,13 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-
-const meals = [
-	"Mexican",
-	"Pizza",
-	"Chili",
-	"Nachos",
-	"Pulled Pork",
-	"Breakfast"
-];
+const { loadOldMenus } = require('./../menu-memory/menu.js');
+const memory = path.resolve(__dirname, "./../menu-memory/meal-ideas.json");
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('get-ideas')
 		.setDescription('Have the bot pitch some meal ideas'),
 	async execute(interaction) {
+		const meals = JSON.parse(fs.readFileSync(memory));
 		let randoIdea = meals[Math.floor(Math.random() * meals.length)]
 		
 		return interaction.reply(`How about ${randoIdea} for dinner?`);
