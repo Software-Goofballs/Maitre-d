e14241d5952d7ddf7a0d7676b31851764999472d
TheENGR <50523916+TheENGR@users.noreply.github.com>
2022-03-22 18:53:33 -0500

    Create add-dinner-idea.js

---
+++ commands/add-dinner-idea.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const memory = path.resolve(__dirname, "./../menu-memory/meal-ideas.json");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('add-dinner-idea')
+		.setDescription('Add dinner ideas for the bot to pitch'),
+  .addStringOption(option => option.setName('idea').setDescription('Enter the name of the meal').setRequired(true))
+	async execute(interaction) {
+		let meals = JSON.parse(fs.readFileSync(memory));
+    let idea = interaction.options.getString('idea');
+    meals.push(idea);
+    fs.writeFileSync(memory, JSON.stringify(meals, undefined, 4));
+		return interaction.reply(`Meal idea added! ${idea}`);
+	},
+};
