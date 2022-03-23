9e74b980151ced1228f6ca8e5d04801362e94874
TheENGR <50523916+TheENGR@users.noreply.github.com>
2022-03-22 19:14:46 -0500

    Update get-ideas.js

--- commands/get-ideas.js
+++ commands/get-ideas.js
@@ -5,11 +5,20 @@ const memory = path.resolve(__dirname, "./../menu-memory/meal-ideas.json");
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('get-ideas')
-		.setDescription('Have the bot pitch some meal ideas'),
+		.setDescription('Have the bot pitch some meal ideas')
+		.addNumberOption(option => option.setName('number-of-ideas').setDescription('How many ideas do you need?'))
+		.addNumberOption(option => option.setName('number-of-past-meals-to-skip').setDescription('How many meals in the past do you not want to repeat?')),
 	async execute(interaction) {
+		const numIdeas = interaction.options.getNumber('number-of-ideas');
+		const numToSkip = interaction.options.getNumber('number-of-past-meals-to-skip');
+		
 		const meals = JSON.parse(fs.readFileSync(memory));
-		const shuffled = meals.sort(() => 0.5 - Math.random());
-		let selected = shuffled.slice(0, 3);
+		const oldMenu = loadOldMenus();
+		
+		const lastXSessions = oldMenu.slice(0, numIdeas ? numIdeas : 3).map(x => x.Main);
+		const shuffled = meals.filter(x => !lastXSessions.includes(x))
+			.sort(() => 0.5 - Math.random());
+		let selected = shuffled.slice(0, numIdeas ? numIdeas : 3);
 		
 		return interaction.reply(`How about ${selected.join(" or ")} for dinner?`);
 	},
