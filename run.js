54fe184babedfdb74fc53a7d0722d10cc6cc4abc
TheENGR <TheENGR42@gmail.com>
2023-04-20 20:08:19 -0500

    Memory

--- run.js
+++ run.js
@@ -1,12 +1,13 @@
 const { execSync } = require('child_process');
 
 (async () => {
-	await execSync('git pull')
 	try {
+		await execSync('git pull')
 		await execSync('git add menu-memory/*.json')
 		await execSync('git commit -m "Save Memory"')
 		await execSync('git push')
-	} catch {
+	} catch (e) {
+		console.log(e)
 	}
 	await execSync('npm ci')
 	await require('./deploy-commands.js')
